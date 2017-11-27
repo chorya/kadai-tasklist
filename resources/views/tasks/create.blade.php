5bed05ec50d0f229387fc451d3aadd614ada5f5a
tetsuya chiba <chiba1220wjuken@gmail.com>
2017-11-27 07:48:38 +0000

    bootstrap design

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,25 @@
 
     <h1>タスク新規作成ページ</h1>
 
+    <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
     {!! Form::model($task, ['route' => 'tasks.store']) !!}
-
-        {!! Form::label('status', 'ステータス:') !!}
-        {!! Form::text('status') !!}
-
-        {!! Form::label('content', 'タスク:') !!}
+        <div class="form-group col-xs-12 col-sm-8 col-md-8 col-lg-6">
+            {!! Form::label('status', 'ステータス:') !!} <br>
+            {!! Form::text('status') !!}
+        </div>
+        
+        <div class="form-group col-xs-12 col-sm-8 col-md-8 col-lg-6">
+        {!! Form::label('content', 'タスク:') !!} <br>
         {!! Form::text('content') !!}
+        </div>
+        
+        <div class="col-xs-12 col-sm-8 col-md-8 col-lg-6">
+        {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
+        </div>
 
-        {!! Form::submit('投稿') !!}
-
-    {!! Form::close() !!}
+        {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -3,14 +3,25 @@
 @section('content')
 
     <h1>id = {{ $task->id }} のメッセージ詳細ページ</h1>
-    
-    <p>ステータス: {{ $task->status }}</p>
-    <p>タスク: {{ $task->content }}</p>
+    <table class="table table-bordered container">
+        <tr class="row">
+            <th class="col-lg-6 col-md-6 col-sm-6 col-xs-6">id</th>
+            <td class="col-lg-6 col-md-6 col-sm-6 col-xs-6">{{ $task->id }}</td>
+        </tr>
+        <tr class="row">
+            <th class="col-lg-6 col-md-6 col-sm-6 col-xs-6">ステータス</th>
+            <td class="col-lg-6 col-md-6 col-sm-6 col-xs-6">{{ $task->status }}</td>
+        </tr>
+        <tr class="row">
+            <th class="col-lg-6 col-md-6 col-sm-6 col-xs-6">タスク</th>
+            <td class="col-lg-6 col-md-6 col-sm-6 col-xs-6">{{ $task->content }}</td>
+        </tr>
+    </table>
 
-    {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id]) !!}
+    {!! link_to_route('tasks.edit', 'このタスク編集', ['id' => $task->id], ['class' => 'btn btn-default']) !!}
     
      {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
-        {!! Form::submit('削除') !!}
+        {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
 
 @endsection
