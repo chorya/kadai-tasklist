fe05e6647cc01402f77d831cdce924b341dee869
tetsuya chiba <chiba1220wjuken@gmail.com>
2017-12-02 06:38:20 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,14 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/', 'TasksController@index');
 
 Route::resource('tasks', 'TasksController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
