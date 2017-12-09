28767c66df16d0be4f27389337bc145ea381a428
tetsuya chiba <chiba1220wjuken@gmail.com>
2017-12-09 02:34:43 +0000

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -25,5 +25,5 @@ Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
-    Route::resource('tasks', 'TasksController');
+    Route::resource('tasks', 'TasksController', ['only' => ['store', 'destroy','update']]);
 });
